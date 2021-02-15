fa654eabb310b8a03016df021e3bf1df855e3076
imran sk <imran.bu96@gmail.com>
2021-02-15 22:18:52 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("AppBar Text"),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+          child: Text(
+        "Imran sk ",
+        style: TextStyle(
+          fontSize: 40.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.red[600],
+          fontFamily: "BalletFont"
+
+        ),
+      )),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red[600],
+        onPressed: () {
+          print("Hello");
+        },
+        child: Text("Button"),
+      ),
+    ));
+  }
+}
